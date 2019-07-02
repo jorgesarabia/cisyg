5466c42f212fd139f1dba083f542d083fd871854
Jorge Sarabia <ingjorgesarabia@gmail.com>
2019-07-02 20:16:00 -0300

    Se empieza a utilizar el blueprint

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 DEBUG = True
+
+## Para tener de respaldo
+# import os
+
+# os.environ["LC_ALL"] = "C.UTF-8"
+# os.environ["LANG"] = "C.UTF-8"
+# os.environ["FLASK_APP"] = "mYapp.py"
--- home/routes.py
+++ home/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+from flask import render_template
 
 home_mod = Blueprint('home',__name__)
 
 @home_mod.route('/')
 def index():
-    return '<h1>Estas en el home</h1>'
+    return render_template('hello.html')
